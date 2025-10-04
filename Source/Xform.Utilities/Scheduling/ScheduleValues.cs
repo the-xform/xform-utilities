f89fe8d0fa3f2809b30ba7ab0d0530289f7b8631
Ruchika <ruchika@neuro-spinal.com>
2025-10-03 20:41:39 -0700

    Added nuget configuration to project file.

--- Source/Xform.Utilities/Scheduling/ScheduleValues.cs
+++ Source/Xform.Utilities/Scheduling/ScheduleValues.cs
@@ -3,56 +3,6 @@
 // This file is licensed under the MIT License.
 // See the LICENSE file in the project root for details.
 
-#if SAMPLE
-
-	"Schedule":
-	{
-		"Type": "ENUM: Interval | OneShot | Daily | Weekly | Monthly",
-   		"DateScheduleStart": "Optional date when schedule starts. Empty implies now.",
-   		"DateScheduleStop": "Optional date if schedule has a hard stop in the future.",
-   		"Interval":
-   		{
-			"TimeStart": "00:00:00.000", // Initial start delay
-			"Duration": "0.00:00:00.00" // ... then every X.
-		},
-		"OneShot":
-		{
-			"TimeStart": "00:00:00.000" // Initial start delay, then go.
-
-		}
-		"Daily":
-		{
-			"TimeStart": "00:00:00.000",
-   			"Interval": "1 is every day, 2 is every other day, etc.",
-   			"TimesOfTheDay": // Optional: extra times.
-   			[
-				"TimeStart": "00:00:00.000",
-				"TimeStart": "00:00:00.000",
-				"TimeStart": "00:00:00.000"
-   				// ...more times here
-   			]
-		}
-		"Weekly":
-		{
-			"TimeStart": "00:00:00.000",
-			"Interval": "1 is every week, 2 is every other week, etc.",
-			"DaysOfTheWeek": "Every day if empty. Comma separated otherwise. Like: 'Monday, Friday'"
-		}
-		"Monthly":
-		{
-			"TimeStart": "00:00:00.000",
-			"MonthsOfTheYear": "Every month if empty. Comma separated otherwise.",
-			"DayOfTheMonth": // Use 'DayNumber' alone or set it to 0 and use 'OnThe' and 'DayOfTheWeek' together.
-			{
-				"DayNumber": "1 - 31",
-				"OnThe": "First | Second | Third | Fourth | Last",
-				"DayOfTheWeek": "Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday"
-			}
-		}
-	}
-
-#endif
-
 namespace Xform.Utilities.Scheduling;
 
 /// <summary>
